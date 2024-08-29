84b240327347ff7d9e3b0d736d88c2e917001e04
anhquantr123 <anhquan.lien123@gmail.com>
2024-08-29 18:29:54 +0700

    create constant app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color titleColor = Color.fromARGB(0, 22, 22, 22);
+  static const Color textColor = Color(0xffccc7c5);
+  static const Color mainColor = Color(0xff89dad0);
+}
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class BigText extends StatelessWidget {
+  final String text;
+  Color? color;
+  double size;
+  bool fontWeight;
+  TextOverflow overFlow;
+  BigText(
+      {super.key,
+      required this.text,
+      this.color = AppColor.textColor,
+      this.size = 24,
+      this.overFlow = TextOverflow.ellipsis,
+      this.fontWeight = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: size,
+          color: color,
+          overflow: overFlow,
+          fontWeight: fontWeight ? FontWeight.bold : FontWeight.normal),
+    );
+  }
+}
